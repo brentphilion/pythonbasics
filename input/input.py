c535daa44e1ba6a78d8510c8017fb690d550c020
brentphilion <43643086+brentphilion@users.noreply.github.com>
2021-09-17 10:48:47 -0400

    worked on input with group added todo about input controls for next week

--- input/input.py
+++ input/input.py
@@ -1,4 +1,19 @@
 """
 Explain input and try loops for error handling
 
+todo "try and input checking for next week"
+
+"""
+
+name = input("What is your name?")
+
+
+print(f"my name is {name}" )
+
+print(f"{api call} {api argument}" )
+#print('my name is ', name)
+
+"""
+api_call = "fixed_string"
+api_argument = (f"fixed part{variable1} more fixed {variable2} nd more formoeeting as needed")
 """
